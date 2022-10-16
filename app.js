06afdcc3a9504b03d1f8b89081e3567866f43bb5
Andrew <zhengandrew35@gmail.com>
2022-10-15 23:43:55 -0400

    Added ABCD buttons

--- app.js
+++ app.js
@@ -4,10 +4,6 @@ const url = "https://animechan.vercel.app/"
 let fakeAnswers = [{name2:'Ichicgo Kurosaki', anime2: 'Bleach'}, {name2:'Hamtaro', anime2: 'Hamtaro'},{name2:'Saitama', anime2: 'One Punch man'},{name2:'Shinji Ikari', anime2: 'Neon Genesis Evangelion'}, {name2:'Edward Elric', anime2: 'Full Metal Alchemist'}, {name2:'Mai valentine', anime2: 'Yugioh Duel Monsters'}, {name2:'Goku', anime2: 'Dragonball Z'}, {name2:'Yamcha', anime2: 'Dragonball'}, {name2:'Paradox Brothers', anime2: 'Yugioh GX'}, {name2:'Yu Narukami', anime2: 'Persona 4: The Animation'}, {name2:'Adachi', anime2: 'Persona 4: The Golden Animation'}, {name2:'Satsuki Kiryuin', anime2: 'Kill la Kill'}, {name2:'Ryuko Matoi', anime2: 'Kill la Kill'}, {name2:'', anime2: 'Kill la Kill'}]
 
 
-//for (let i=0; i<fakeAnswers.length; i++){
-    //console.log(fakeAnswers[i].name2);
-//}
-
 function randomFakeAnswer(){
   let y = Math.floor((Math.random()*4))
     
@@ -15,9 +11,6 @@ function randomFakeAnswer(){
 }
 
 
-
-
-//testing if the link works 
 fetch(url)
   .then(res => {
     console.log("success!", res);
@@ -27,13 +20,6 @@ fetch(url)
   });
 
 
-  //testing the api
-  //fetch('https://animechan.vercel.app/api/random')
-   // .then(response => response.json())
-   // .then(quote => console.log(quote.character))
-
-
-// Question button logic here
 let answer = false 
 let button = document.querySelector('#nextQuestion')
 
@@ -47,12 +33,9 @@ let placementB =document.querySelector('#character2')
 let placementC =document.querySelector('#character3')
 let placementD =document.querySelector('#character4')
 
-let placementLogic = [placement1, placement2, placement3, placement4] //anime
+let placementLogic = [placement1, placement2, placement3, placement4] 
 let placementLogic2 =[placementA,placementB,placementC,placementD]
 
-//character
-
-//let placementLogic = [{array1: placement1, array2: placementA}, {array1: placement2, array2: placementB},{array1: placement3, array2: placementC} , {array: placement4, array2: placementD} ]
 
 
 function placementSelector(){
@@ -68,14 +51,6 @@ function randomFakeAnswer(){
   return fakeAnswers[Math.floor((Math.random()*15))]
 }
 
-
-//function placementSelector2(){
-  //let x = Math.floor((Math.random()*4))
-  ///let randomPlacement= x
-
-  //return placementLogic2[randomPlacement] 
-
-//}
 
 function randomFakeAnswer(){
   return fakeAnswers[Math.floor((Math.random()*15))]
@@ -188,9 +163,22 @@ function correctAnswer(res){
 }
 
 
+let buttonA = document.querySelector('#buttonA').addEventListener('click', ()=>{
+console.log ('A')
+})
 
 
+let buttonB = document.querySelector('#buttonB').addEventListener('click', ()=>{
+  console.log ('B')
+})
 
+let buttonC = document.querySelector('#buttonC').addEventListener('click', ()=>{
+    console.log ('C')
+  })
+
+let buttonD = document.querySelector('#buttonD').addEventListener('click', ()=>{
+    console.log ('D')
+})
 
 
 
@@ -215,17 +203,9 @@ async function getQuestion (){
 
 button.addEventListener('click', getQuestion)
     
-// Decided that character and anime will be A B C D
-//quote will be the only thing shown on question
 
 
-//need a method to swap around A B C D
-//first see if different calls can be made
 
-
-// actually i realized I can literally just make an array with fake answers
-
-//map the divs. tell api call to put it in any one randomly. mathfloor() 
 
 // add a function to pick random name in a random spot. It cannot equal the placement of the answer. It cannot equal the intnertext of the answer. 
 
